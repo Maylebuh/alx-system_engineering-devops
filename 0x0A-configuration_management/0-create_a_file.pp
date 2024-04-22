cbd79914f84b30c2a037eec3c389454906d17b9b
Maylebuh <malebomawela96@gmail.com>
2024-04-22 11:47:28 -0700

    0x0A. Configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# cerates a file
+# Creates a file
 file { '/tmp/school':
-	mode	 => '0744',
-	owner	 => 'www-data',
-	group	 => 'www-data',
-	content => 'I love Puppet'
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
-
